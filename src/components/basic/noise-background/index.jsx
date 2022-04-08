2e1df13ffbc5b0042a8b9673a3e766b73de20733
Benjamin Brachmann <benjamin.brachmann@icloud.com>
2022-04-09 01:16:48 +0200

    - changed noise background to a functional component

--- src/components/basic/noise-background/index.jsx
+++ src/components/basic/noise-background/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 /* Styles */
 import './noise-background.scss'
@@ -6,45 +6,40 @@ import './noise-background.scss'
 /* Modules */
 import Noise from './noise-background.module'
 
-class NoiseBackground extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      canvasReference: React.createRef(),
+const NoiseBackground = function ({ color, children }) {
+  // Component state
+  const [noise, setNoise] = useState(null)
+  const canvasRef = React.createRef()
+
+  // Component effects
+  const startNoise = () => {
+    const noiseInstance = new Noise({
+      canvas: canvasRef.current,
+      color: color,
+    })
+    noiseInstance.start()
+    console.log(noiseInstance)
+    setNoise(noiseInstance)
+  }
+
+  const endNoise = () => {
+    if (noise) {
+      noise.pause()
     }
   }
 
-  componentDidMount() {
-    const { canvasReference } = this.state
+  useEffect(() => {
+    startNoise()
+    return endNoise
+  }, [])
 
-    const noise = new Noise({
-      canvas: canvasReference.current,
-      color: this.props.color,
-    })
-
-    noise.start()
-
-    this.setState({
-      noise,
-    })
-  }
-
-  componentWillUnmount() {
-    if (this.state.noise) {
-      this.state.noise.pause()
-    }
-  }
-
-  render() {
-    const { children } = this.props
-
-    return (
-      <div className="app-noise-background-container">
-        <canvas ref={this.state.canvasReference} className="app-noise-background-canvas" width="0" height="0" />
-        <div className="app-noise-background-container-content">{children}</div>
-      </div>
-    )
-  }
+  // Component render
+  return (
+    <div className="app-noise-background-container">
+      <canvas ref={canvasRef} className="app-noise-background-canvas" width="0" height="0" />
+      <div className="app-noise-background-container-content">{children}</div>
+    </div>
+  )
 }
 
 export default NoiseBackground
